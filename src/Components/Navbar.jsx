58b3584a7ea690d30bb765f26de6d4e3e2e97482
MALIK ABDUL BASIT <malikabdulbasit03@gmail.com>
2025-06-03 14:42:17 +0500

    Fixed navbar issue

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -74,13 +74,13 @@ const Navbar = ({ relative, hidden, classic }) => {
                 key={item.id}
                 href={item.url}
                 onClick={handleClick}
-                className={`block uppercase relative font-code text-2xl text-n-1 transition-colors hover:text-color-1 cursor-pointer ${
+                className={`block uppercase relative font-code text-2xl text-n-1 transition-colors cursor-pointer ${
                   item.onlyMobile ? "lg:hidden" : ""
                 } ${
-                  item.url === pathname.hash
-                    ? "z-2 lg:text-n-1"
-                    : "lg:text-n-1/50"
-                } font-poppins px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold lg:leading-5 lg:hover:text-orange-500 xl:px-12`}
+                  item.url === pathname
+                    ? "z-2 lg:text-orange-500"
+                    : "lg:text-n-1"
+                } font-poppins px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold lg:leading-5  lg:hover:text-orange-500 xl:px-12`}
               >
                 {item.clerk ? (
                   user ? (
